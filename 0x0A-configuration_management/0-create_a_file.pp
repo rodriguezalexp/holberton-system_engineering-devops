09a34ae751962d758aad6269bc1db04cc547a910
rodriguezalexp <rodriguezalexp@github.com>
2022-01-04 04:28:06 +0000

    tasks sucessfull

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+#new  file
+
+file { 'holberton':
+    path    => '/tmp/school',
+    mode    => '0744',
+    owner   => 'www-data',
+    group   => 'www-data',
+    content => 'I love Puppet',
+}
---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,6 @@
+# puppet-lint instalation
+
+package {'puppet-lint':
+    ensure: => '2.1.1',
+    provider => 'gem',
+}
