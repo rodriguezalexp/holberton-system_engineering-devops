8721052b239a972fd209b29caddc4f545c5c27f5
Alexander <rodriguezalexp@github.com>
2022-01-18 21:24:12 +0000

    update changes

---
+++ 0x0F-load_balancer/1-install_load_balancer.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+#configuration of the load balancer
+sudo apt-get -y update
+sudo apt-get -y install haproxy
+sudo chmod 777 /etc/haproxy/haproxy.cfg 
+echo "frontend firtslb
+	bind *:80
+	option forwardfor
+	default_backend holbi-servers
+	
+backend holbi-servers
+	balance roundrobin
+	server 2618-web-01 104.196.141.92:80 check
+	server 2618-web-02 75.101.202.218:80 check" > /etc/haproxy/haproxy.cfg
+sudo service nginx restart
+
