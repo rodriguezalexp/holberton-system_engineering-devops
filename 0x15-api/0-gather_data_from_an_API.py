cd99aabbc229643b6ef5349d358b94db1fb532d1
Alexander <rodriguezalexp@github.com>
2022-02-01 20:36:58 +0000

    API'S project

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+# Python script that, using this REST API, for a given employee ID,
+import requests
+from sys import argv
+
+
+if __name__ == '__main__':
+
+    UserId = argv[1]
+    User = requests.get('https://jsonplaceholder.typicode.com/users/{}'.
+                        format(UserId)).json()
+
+    TODO = requests.get('https://jsonplaceholder.typicode.com/todos?userId={}'.
+                        format(UserId)).json()
+
+    DONE_TASKS = requests.get('https://jsonplaceholder.typicode.com/'
+                              'todos?userId={}&&completed=true'.
+                              format(UserId)).json()
+
+    EMPLOYEE_NAME = User['name']
+    COMPLETED_TASK = len(DONE_TASKS)
+    TOTAL_TASKS = len(TODO)
+
+    print('Employee {} is done with tasks({}/{}):'.
+          format(EMPLOYEE_NAME, COMPLETED_TASK, TOTAL_TASKS))
+
+    for i in DONE_TASKS:
+        print('\t {}'.format(i['title']))
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # 0x0D-web_stack_debugging_0
+this is an introductory project to learn about API'S
+
+## General
+*What is an API
+*What is a REST API
+*What are microservices
+*What is the CSV format
+*What is the JSON format
