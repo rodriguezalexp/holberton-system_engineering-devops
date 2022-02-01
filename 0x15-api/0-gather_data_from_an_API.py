504df8d22a9df569dafa0d503c016c4b037b4d28
Alexander <rodriguezalexp@github.com>
2022-02-01 20:42:42 +0000

    pep8

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-""" 
-    Python script that, using this REST API, for a given employee ID
 """
+Python script that, using this REST API, for a given employee ID
+"""
+
 import requests
 from sys import argv
 
