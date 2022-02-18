bd7968775ba23c68f05487c84611fc8467584617
rodriguezalexp <alex0894rp@gmail.com>
2022-02-18 04:49:08 +0000

    top ten

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""""""
+import requests
+
+
+def top_ten(subreddit):
+    headers = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64)\
+                                               AppleWebKit/537.36 \
+                   (KHTML, like Gecko) Chrome/98.0.4758.87 Safari/537.36"}
+
+    url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.\
+        format(subreddit)
+    pop = requests.get(url, headers=headers)
+    poppop = pop.json().get('data').get('children')
+
+    if poppop.status_code == 200:
+        for i in poppop:
+            print(i.get('data').get('title'))
+        return 0
