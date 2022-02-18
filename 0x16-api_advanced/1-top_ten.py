e9c2084a2b478b5e2b38b4c7350dc198f4c905cf
rodriguezalexp <alex0894rp@gmail.com>
2022-02-18 05:34:08 +0000

    nu change

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -5,6 +5,7 @@ import requests
 
 
 def top_ten(subreddit):
+    """function to find post subreddits"""
     headers = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64)\
                                                AppleWebKit/537.36 \
                    (KHTML, like Gecko) Chrome/98.0.4758.87 Safari/537.36"}
