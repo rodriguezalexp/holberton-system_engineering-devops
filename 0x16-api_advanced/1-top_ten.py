e2dd658a68125e07846a298915728566bf44bbef
rodriguezalexp <alex0894rp@gmail.com>
2022-02-18 04:55:33 +0000

    add changes

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -9,11 +9,12 @@ def top_ten(subreddit):
                    (KHTML, like Gecko) Chrome/98.0.4758.87 Safari/537.36"}
 
     url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.\
-        format(subreddit)
-    pop = requests.get(url, headers=headers)
+           format(subreddit)
+    pop = requests.get(url, headers=headers, allow_redirects=False)
     poppop = pop.json().get('data').get('children')
 
-    if poppop.status_code == 200:
+    if pop.status_code == 200:
         for i in poppop:
             print(i.get('data').get('title'))
-        return 0
+    else:
+        print("None")
